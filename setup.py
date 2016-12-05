179f138b954b333b454dcc7396757733a4143e9d
Dzianis Dashkevich <dskecse@gmail.com>
2016-12-05 16:38:43 +0300

    Add setup.py to allow installation using pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gaesessions',
+    version='1.07',
+    author='David Underhill',
+    author_email='',
+    license='Apache License Version 2.0',
+    description='A sessions library for the Python runtime on Google App Engine',
+    py_modules=['gaesessions.__init__'],
+    install_requires=['setuptools']
+)
